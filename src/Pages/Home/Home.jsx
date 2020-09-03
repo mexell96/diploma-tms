e3d734b21033c1aedc289f169a16f8ec1c8acc90
Gleb Rosev <rossevgleb@gmail.com>
2020-09-03 19:28:13 +0300

    Added pagination

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,38 +1,43 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getShows } from "../../actions/shows";
-import ShowsGallery from "./components/ShowsGallery"
-import {makeStyles} from "@material-ui/styles"
-import {Button} from "@material-ui/core"
+import ShowsGallery from "./components/ShowsGallery";
+import { makeStyles } from "@material-ui/styles";
+import { Button } from "@material-ui/core";
+import { arrayFromNumber } from "./utils/index";
 
 const useStyles = makeStyles({
   root: {
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center"
-  }
-})
+    justifyContent: "center",
+  },
+});
+
+const pages = 12;
 
 function Home(props) {
-  const classes = useStyles()
+  const classes = useStyles();
   useEffect(() => {
     props.getShows(1);
   }, [props]);
 
-
-  const handlePageChange = page => e => {
+  const handlePageChange = (page) => (e) => {
     props.getShows(page);
-  }
+  };
 
-  return <div className={classes.root}><ShowsGallery /> 
-  <div>
-     {
-    [1,2].map(number=><Button 
-    onClick={handlePageChange(number)}
-    >{number}</Button>)
-  } 
-  </div>
- </div>;
+  return (
+    <div className={classes.root}>
+      <ShowsGallery />
+      <div>
+        {arrayFromNumber(pages)
+          .map((page) => page + 1)
+          .map((number) => (
+            <Button onClick={handlePageChange(number)}>{number}</Button>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => ({});
