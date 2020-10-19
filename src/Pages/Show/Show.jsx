e5a3752f7727f61b94dbe82ec21fd338dce9ab2e
artem <mexell96@gmail.com>
2020-10-19 12:26:28 +0300

    added style Show

--- src/Pages/Show/Show.jsx
+++ src/Pages/Show/Show.jsx
@@ -1,10 +1,27 @@
-import React, {useEffect} from 'react'
-import {withRouter} from "react-router-dom"
-import {connect} from "react-redux"
-import {getShow} from "../../actions/shows.js"
-import ShowCard from '../../components/ShowCard'
+import React, {useEffect} from 'react';
+import {withRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import {getShow} from "../../actions/shows.js";
+import ShowCard from '../../components/ShowCard';
+import background from "../../images/background.jpg";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  backgr: {
+    backgroundImage: `url(${background})`,
+  },
+  mainBG : {
+    display: "flex",
+    width: 1110,
+    margin: "0 auto",
+    background: "white",
+    padding: 30,
+  }
+}));
 
 function Show(props) {
+  const classes = useStyles();
+
   useEffect(() => {
     props.getShow(props.match.params.id)
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -12,15 +29,16 @@ function Show(props) {
 
   console.log(props)
   return (
-    <div>
-
-    {props.show && 
-      <ShowCard src={props.show.image ? props.show.image.medium : "" } 
-      id={props.show.id} 
-      title={props.show.name}
-      description={props.show.summary}
-      >
-      </ShowCard>}
+    <div className={classes.backgr}>
+      <div className={classes.mainBG}>
+      {props.show && 
+        <ShowCard src={props.show.image ? props.show.image.medium : "" } 
+        id={props.show.id} 
+        title={props.show.name}
+        description={props.show.summary}
+        >
+        </ShowCard>}
+      </div>
     </div>
   )
 }
